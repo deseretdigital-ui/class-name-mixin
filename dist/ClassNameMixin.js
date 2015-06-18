4d096e891aa17691a8adba787af11d0318a0b870
dahei <heidecke@gmail.com>
2015-06-18 11:15:27 +0200

    use unminified version

--- dist/ClassNameMixin.js
+++ dist/ClassNameMixin.js
@@ -1,6 +1,212 @@
-!function(e,t){if("object"==typeof exports&&"object"==typeof module)module.exports=t(require("react/addons"));else if("function"==typeof define&&define.amd)define(["react/addons"],t);else{var r=t("object"==typeof exports?require("react/addons"):e.React);for(var o in r)("object"==typeof exports?exports:e)[o]=r[o]}}(this,function(e){return function(e){function t(o){if(r[o])return r[o].exports;var n=r[o]={exports:{},id:o,loaded:!1};return e[o].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var r={};return t.m=e,t.c=r,t.p="",t(0)}([function(e,t,r){e.exports=r(1)},function(e,t,r){var o=r(2),n=r(3),s=r(4),a=r(5),i={propTypes:{className:o.PropTypes.oneOfType([o.PropTypes.string,o.PropTypes.object])},getDefaultProps:function(){return{className:{}}},getClassName:function(e){void 0===e&&(e={}),"string"==typeof e&&(e=n(e));var t=this.props.className;"string"==typeof t&&(t=n(t));var r={};"function"==typeof this.getComponentClassName&&(r=this.getComponentClassName(),"string"==typeof r&&(r=n(r)));var o=s(r,e);return o=s(o,t),a(o)}};e.exports=i},function(t,r){t.exports=e},function(e,t){e.exports=function(e){var t={};return e.split(" ").forEach(function(e){t[e]=!0}),t}},function(e,t){e.exports=function(e,t){var r=JSON.parse(JSON.stringify(e));for(className in t)t.hasOwnProperty(className)&&(r[className]=t[className]);return r}},function(e,t,r){var o;/*!
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("react/addons"));
+	else if(typeof define === 'function' && define.amd)
+		define(["react/addons"], factory);
+	else {
+		var a = typeof exports === 'object' ? factory(require("react/addons")) : factory(root["React"]);
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, function(__WEBPACK_EXTERNAL_MODULE_2__) {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var React = __webpack_require__(2);
+	var convertStringToClassSetObject = __webpack_require__(3);
+	var mergeClassSetObjects = __webpack_require__(4);
+	var classNames = __webpack_require__(5);
+
+	var ClassNameMixin = {
+	  propTypes: {
+	    'className': React.PropTypes.oneOfType([
+	      React.PropTypes.string,
+	      React.PropTypes.object
+	    ])
+	  },
+
+	  getDefaultProps: function() {
+	    return {
+	      'className': {}
+	    };
+	  },
+
+	  getClassName: function(className) {
+	    if (className === undefined) {
+	      className = {};
+	    }
+
+	    if (typeof className === 'string') {
+	      className = convertStringToClassSetObject(className)
+	    }
+
+	    var propClassName = this.props.className;
+	    if (typeof propClassName === 'string') {
+	      propClassName = convertStringToClassSetObject(propClassName)
+	    }
+
+	    var componentClassName = {};
+	    if (typeof this.getComponentClassName === 'function') {
+	      componentClassName = this.getComponentClassName();
+
+	      if (typeof componentClassName === 'string') {
+	        componentClassName = convertStringToClassSetObject(componentClassName)
+	      }
+	    }
+
+	    var mergedClassName = mergeClassSetObjects(componentClassName, className);
+	    mergedClassName = mergeClassSetObjects(mergedClassName, propClassName);
+
+	    return classNames(mergedClassName);
+	  }
+	};
+
+	module.exports = ClassNameMixin;
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	module.exports = __WEBPACK_EXTERNAL_MODULE_2__;
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports = function(classNames) {
+	  var classSetObject = {};
+
+	  classNames.split(' ').forEach(function(className) {
+	    classSetObject[className] = true;
+	  });
+
+	  return classSetObject;
+	};
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	module.exports = function(baseObject, objectToMerge) {
+	  var clonedObject = JSON.parse(JSON.stringify(baseObject));
+
+	  for (className in objectToMerge ) {
+	    if (objectToMerge.hasOwnProperty(className)) {
+	      clonedObject[className] = objectToMerge[className];
+	    }
+	  }
+
+	  return clonedObject;
+	};
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var __WEBPACK_AMD_DEFINE_RESULT__;/*!
 	  Copyright (c) 2015 Jed Watson.
 	  Licensed under the MIT License (MIT), see
 	  http://jedwatson.github.io/classnames
 	*/
-!function(){"use strict";function n(){for(var e="",t=0;t<arguments.length;t++){var r=arguments[t];if(r){var o=typeof r;if("string"===o||"number"===o)e+=" "+r;else if(Array.isArray(r))e+=" "+n.apply(null,r);else if("object"===o)for(var s in r)r.hasOwnProperty(s)&&r[s]&&(e+=" "+s)}}return e.substr(1)}o=function(){return n}.call(t,r,t,e),!(void 0!==o&&(e.exports=o))}()}])});
+
+	(function () {
+		'use strict';
+
+		function classNames () {
+
+			var classes = '';
+
+			for (var i = 0; i < arguments.length; i++) {
+				var arg = arguments[i];
+				if (!arg) continue;
+
+				var argType = typeof arg;
+
+				if ('string' === argType || 'number' === argType) {
+					classes += ' ' + arg;
+
+				} else if (Array.isArray(arg)) {
+					classes += ' ' + classNames.apply(null, arg);
+
+				} else if ('object' === argType) {
+					for (var key in arg) {
+						if (arg.hasOwnProperty(key) && arg[key]) {
+							classes += ' ' + key;
+						}
+					}
+				}
+			}
+
+			return classes.substr(1);
+		}
+
+		if (true) {
+			// AMD. Register as an anonymous module.
+			!(__WEBPACK_AMD_DEFINE_RESULT__ = function () {
+				return classNames;
+			}.call(exports, __webpack_require__, exports, module), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+		} else if (typeof module !== 'undefined' && module.exports) {
+			module.exports = classNames;
+		} else {
+			window.classNames = classNames;
+		}
+
+	}());
+
+
+/***/ }
+/******/ ])
+});
+;
